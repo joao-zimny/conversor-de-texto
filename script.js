d99ceeb9838a5d667c8e2c03e4b8586cb9a99286
João Guilherme <zimnyjoao@outlook.com>
2023-10-05 00:39:46 -0300

    adicionando o botao de limpar o texto digitado

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const element = document.querySelector('#text-area');
 
 function uppercase() {
-    const element = document.querySelector('#text-area');
     element.value = element.value.toUpperCase();
 }
 
 function lowercase() {
-    const element = document.querySelector('#text-area');
     element.value = element.value.toLowerCase();
 }
 
 function capitalize() {
-    const element = document.querySelector('#text-area');
     const words = element.value.split(' ');
     const capitalize = words.map(function(word) {
         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
@@ -20,24 +17,23 @@ function capitalize() {
 }
 
 function removeSpace() {
-    const element = document.querySelector('#text-area');
     const text = element.value;
     element.value = text.split(' ').join("");
 }
 
 function copy() {
-    const element = document.querySelector('#text-area');
-    var texto = document.getElementById('text-area');
-    texto.select();
+    element.select();
     document.execCommand('copy');
 
-    // Pop-up
-    const modal = document.querySelector('dialog');
-    modal.show();
-    const modalClose = document.querySelector('dialog button');
-    modalClose.onclick = function() {
-        modal.close();
-    }
-    
+    // TODO: Pop-up
+    // const modal = document.querySelector('dialog');
+    // modal.show();
+    // const modalClose = document.querySelector('dialog button');
+    // modalClose.onclick = function() {
+    //     modal.close();
+    // }
 }
 
+function clean() {
+    element.value = "";
+}
